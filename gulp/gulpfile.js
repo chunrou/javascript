112f01d031cd4796b076d912921466b0180c8879
DK <329865849@qq.com>
2018-06-27 23:43:00 +0800

    Update gulpfile.js

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,6 +4,10 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     connect = require('gulp-connect');//livereload
+    var sass = require('gulp-sass');
+    var mincss = require('gulp-clean-css');
+    
+var amdOptimize = require("amd-optimize"); 
    
 var jsSrc = 'src/js/*.js';
 var jsDist = 'dist/js';
@@ -15,13 +19,39 @@ var htmlDist = 'dist';
 gulp.task('js', function () {
 
     gulp.src(jsSrc)
-        .pipe(concat('main.js'))
+        // .pipe(concat('main.js'))
         .pipe(gulp.dest(jsDist))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDist))
+        // .pipe(rename({suffix: '.min'}))
+        // .pipe(uglify())
+        // .pipe(gulp.dest(jsDist))
         .pipe(connect.reload())
 
+});
+
+gulp.task('rjs', function () {
+    gulp.src('./src/js/*.js')
+        .pipe(amdOptimize("main", {
+            paths: {
+                "a": "./src/js/a",
+                "b": "./src/js/b"
+            }
+        }))
+        .pipe(concat("index.js"))           //合并
+        .pipe(gulp.dest("dist/js"))          //输出保存
+        .pipe(rename("index.min.js"))          //重命名
+        .pipe(uglify())                        //压缩
+        .pipe(gulp.dest("dist/js"));         //输出保存
+
+    });
+
+gulp.task('sass', function(){
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(mincss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(connect.reload())
 });
 
 //定义html任务
@@ -41,15 +71,17 @@ gulp.task('connect', function () {
 });
 
 
+
 //定义看守任务
 gulp.task('watch', function () {
 
     gulp.watch('src/*.html', ['html']);
 
-    gulp.watch('src/js/*.js', ['js']);
+    gulp.watch('src/js/*.js', ['rjs']);
 
+    gulp.watch('src/scss/*.scss', ['sass']);
 });
 
 
 //定义默认任务
-gulp.task('default', [ 'js', 'html','watch', 'connect']);
+gulp.task('default', [ 'rjs', 'html', 'sass','watch', 'connect']);
